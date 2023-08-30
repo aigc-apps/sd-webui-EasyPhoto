12ef694930cf8044d551356adb4e185d22cfba3a
bubbliiiing <3323290568@qq.com>
2023-08-30 14:24:36 +0800

    fix install

--- install.py
+++ install.py
@@ -2,5 +2,9 @@ import launch
 
 
 if not launch.is_installed("cv2"):
-    print('Installing requirements for Mov2mov')
+    print('Installing requirements for paiya-webui')
     launch.run_pip("install opencv-python", "requirements for opencv")
+
+if not launch.is_installed("tensorflow"):
+    print('Installing requirements for paiya-webui')
+    launch.run_pip("install tensorflow", "requirements for tensorflow")
