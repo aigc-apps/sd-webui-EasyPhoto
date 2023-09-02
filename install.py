8b4adb4bd5e06483b771dba9b5b004db520daab7
bubbliiiiing <3323290568@qq.com>
2023-09-02 23:38:27 +0800

    delete onnxruntime-gpu for less bug

--- install.py
+++ install.py
@@ -9,9 +9,13 @@ if not launch.is_installed("tensorflow-cpu"):
     print('Installing requirements for easyphoto-webui')
     launch.run_pip("install tensorflow-cpu", "requirements for tensorflow")
 
-if not launch.is_installed("onnxruntime-gpu"):
+if not launch.is_installed("onnx"):
     print('Installing requirements for easyphoto-webui')
-    launch.run_pip("install onnxruntime-gpu", "requirements for onnxruntime-gpu")
+    launch.run_pip("install onnx", "requirements for onnx")
+
+if not launch.is_installed("onnxruntime"):
+    print('Installing requirements for easyphoto-webui')
+    launch.run_pip("install onnxruntime", "requirements for onnxruntime")
 
 if not launch.is_installed("ifnude"):
     print('Installing requirements for easyphoto-webui')
