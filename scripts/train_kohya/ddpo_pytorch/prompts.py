c37718ff144e63ed381de5c3941d93ab35880fe2
hkz <hkunzhe24@gmail.com>
2023-09-26 09:21:11 +0800

    Feature: add reinforcement learning as an advanced option for training (#109)
    * add reinforcement learning to further boost Lora model with some preset metrics such as faceid scores
    * fix max_rl_time && update docstring && update default parameters for smaller computing resources
    * fix timeout in Windows && add ddpo lora model selection &  fix bug in import
    
    Co-authored-by: wuziheng <zhoulou.wzh@alibaba-inc.com>
    Co-authored-by: bubbliiiiing <3323290568@qq.com>

---
+++ scripts/train_kohya/ddpo_pytorch/prompts.py
@@ -0,0 +1,7 @@
+"""This file defines customize prompt funtions. The prompt function which takes no arguments
+generates a random prompt from the given prompt distribution each time it is called.
+"""
+
+
+def easyphoto() -> str:
+    return "easyphoto_face, easyphoto, 1person"
