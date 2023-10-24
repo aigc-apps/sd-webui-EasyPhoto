693ca53995571be4dd20fef26de92f7d499e8254
hkunzhe <hkunzhe24@gmail.com>
2023-10-24 11:37:28 +0800

    fix the version of fsspec

--- install.py
+++ install.py
@@ -26,6 +26,11 @@ if not launch.is_installed("diffusers==0.18.2"):
     print('Installing requirements for easyphoto-webui')
     launch.run_pip("install diffusers==0.18.2", "requirements for diffusers")
 
+# Temporarily pin fsspec==2023.9.2. See https://github.com/huggingface/datasets/issues/6330 for details.
+if not launch.is_installed("fsspec==2023.9.2"):
+    print('Installing requirements for easyphoto-webui')
+    launch.run_pip("install fsspec==2023.9.2", "requirements for fsspec")
+
 if platform.system() != 'Windows':
     if not launch.is_installed("nvitop"):
         print('Installing requirements for easyphoto-webui')
