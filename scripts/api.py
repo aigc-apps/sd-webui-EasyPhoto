851654d7a4c128534b933640361f7388768f68ba
Ji Dong <394764337@qq.com>
2023-12-04 09:55:42 +0800

    Fix the incorrect RGBA format when saving images for api (#297)
    * Fix the incorrect RGBA format when saving images for api
    
    * remove the conditional judgment

--- scripts/api.py
+++ scripts/api.py
@@ -48,6 +48,7 @@ def easyphoto_train_forward_api(_: gr.Blocks, app: FastAPI):
         for instance_image in instance_images:
             hash_value = hashlib.md5(instance_image.tobytes()).hexdigest()
             save_path = os.path.join("/tmp", hash_value + ".jpg")
+            instance_image = instance_image.convert('RGB')
             instance_image.save(save_path)
             _instance_images.append({"name": save_path})
         instance_images = _instance_images
